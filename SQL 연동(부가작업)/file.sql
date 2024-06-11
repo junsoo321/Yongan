0a9e396611560459953c7537aff26aadad94502a
undefinded <84854721+junsoo321@users.noreply.github.com>
2024-06-11 16:13:11 +0900

    Update file.sql

--- SQL 연동(부가작업)/file.sql
+++ SQL 연동(부가작업)/file.sql
@@ -13,10 +13,10 @@ create table face_data(
 -- 위 코드는 테이블 생성하는 코드입니다.
 
 SELECT * FROM face_data;
--- 이 코드는 현재 users 테이블에 들어가 있는 데이터 리스트를 출력하는 코드입니다.
+-- 이 코드는 현재 face_data 테이블에 들어가 있는 데이터 리스트를 출력하는 코드입니다.
 
 drop table face_data;
--- 이 코드는 users 테이블 자체를 삭제하는 코드로 이후 첫번째 create코드로 테이블을 다시 생성해야합니다.
+-- 이 코드는 face_data 테이블 자체를 삭제하는 코드로 이후 첫번째 create코드로 테이블을 다시 생성해야합니다.
 
 TRUNCATE table face_data;
--- 이 코드는 users 테이블의 형태는 유지하고 내부 데이터만 삭제하는 코드입니다.
+-- 이 코드는 face_data 테이블의 형태는 유지하고 내부 데이터만 삭제하는 코드입니다.
